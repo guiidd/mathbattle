1c6a23c6d0f3d402792bf4ad608ed6a9cf0c56c3
Azwan <ha.azwan@yahoo.com>
2019-06-30 04:54:31 +0800

    prac git

--- src/App.js
+++ src/App.js
@@ -4,62 +4,66 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { text: 'I am a useless placeholder' };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { text: 'I am a useless placeholder' };
+    }
 
-  render() {
-    return (
+    render() {
+        return (
 
-      <View style={styles.root}>
-        <View style={styles.container}>
-          <View style={styles.hero} />
-        </View>
-        <View style={styles.container}>
-          <View style={styles.villain} />
-          <View style={styles.villain} />
-          <View style={styles.villain} />
-        </View>
+            <View style={styles.root}>
+                <View style={styles.battlefield}>
+                    <View style={styles.container}>
+                        <View style={styles.hero} />
+                    </View>
+                    <View style={styles.container}>
+                        <View style={styles.villain} />
+                        <View style={styles.villain} />
+                        <View style={styles.villain} />
+                    </View>
+                </View>
+                <Text>Battle math hello</Text>
+                <TextInput
+                    style={{ height: 40, width: 250, borderColor: 'gray', borderWidth: 1, }}
+                    onChangeText={(text) => this.setState({ text })}
+                    value={this.state.text}
+                />
+                <Button
+                    onPress={() => console.log(this.state.text)}
+                    title="submit To Me and only me only me"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
 
-        <Text>Battle math hello</Text>
-        <TextInput
-          style={{ height: 40, width: 250, borderColor: 'gray', borderWidth: 1, }}
-          onChangeText={(text) => this.setState({ text })}
-          value={this.state.text}
-        />
-        <Button
-          onPress={() => console.log(this.state.text)}
-          title="submit To Me and only me only me"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  },
-  hero: {
-    height: 40,
-    width: 40,
-    backgroundColor: 'blue'
-  },
-  villain: {
-    height: 40,
-    width: 40,
-    backgroundColor: 'red'
-  },
+    root: {
+        flex: 1,
+        //flexDirection: 'row',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+    },
+    hero: {
+        height: 40,
+        width: 40,
+        backgroundColor: 'blue'
+    },
+    villain: {
+        height: 40,
+        width: 40,
+        backgroundColor: 'red'
+    },
+    battlefield: {
+        flexDirection: 'row'
+    }
 });
 export default App;
